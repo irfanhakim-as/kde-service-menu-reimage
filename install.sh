bb7fded53b5f7b25cc375ac80d015221c2ba5f03
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-08-13 11:30:18 +0800

    Preserve timestamp of source files

--- install.sh
+++ install.sh
@@ -44,9 +44,9 @@ for var in "${required_vars[@]}"; do
     fi
 done
 
-install -m 755 bin/* "${bin_dir}"
-install -m 644 ServiceMenus/*.desktop "${desktop_dir}"
+install -mp 755 bin/* "${bin_dir}"
+install -mp 644 ServiceMenus/*.desktop "${desktop_dir}"
 install -d "${doc_dir}"
-install -m 644 doc/* "${doc_dir}"
+install -mp 644 doc/* "${doc_dir}"
 
 echo "Done!. kde-service-menu-reimage has been installed. Enjoy!"
